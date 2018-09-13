624e3e92f6ae0cf8a8767d7796319ef792619132
jithin <jithinajith11@gmail.com>
2018-09-13 15:38:52 +0530

    welcome user first try

--- routes/welcome.js
+++ routes/welcome.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var router = express.Router();
-/*var welcomeuserRouter = require('./welcomeuser');
-
-
-var app= express();
-
-app.use('/welcome', welcomeuserRouter);*/
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('usres');
+  res.render('welcome');
 });
-
-
 
 module.exports = router;
